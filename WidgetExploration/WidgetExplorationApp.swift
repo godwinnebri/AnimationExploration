1ab873be34bbf5fa374498f385b90e0763dac74a
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-12-23 23:12:20 +0100

    Initial Commit

---
+++ WidgetExploration/WidgetExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetExplorationApp.swift
+//  WidgetExploration
+//
+//  Created by Godwin IE on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
